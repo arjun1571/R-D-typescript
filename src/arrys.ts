d1f52e641c53eae7e3eedf6b133df1711791281b
arjun1571 <arjundasback515@gmail.com>
2024-02-26 21:56:14 +0600

    r & d array typescript

--- src/arrys.ts
+++ src/arrys.ts
@@ -1,1 +1,8 @@
-const  names=["arjun","das","chandra"]
+const  names: string[]=["arjun","das","chandra"];
+const rollNumber: number[]=[10,12,15,17];
+const mixed = ["name",4,true]
+
+
+// tuple   
+const user: [number,string] =[401,"arjun"]
+const tuple: [string,boolean] =["arjun",true]
